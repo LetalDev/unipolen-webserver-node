266c2305b4c5227c6c8d145d444cb755655509df
logemii <emilybertollo@outlook.com>
2023-11-30 23:04:56 -0300

    hotfix

--- models/adminUser.js
+++ models/adminUser.js
@@ -3,7 +3,8 @@ const { sequelize } = require("../database");
 
 const AdminUser = sequelize.define("AdminUser", {
   id: {
-    type: DataTypes.TEXT,
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
     allowNull: false,
     primaryKey: true,
   }
